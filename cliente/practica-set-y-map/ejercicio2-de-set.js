20f14b0b586a91f87e71e9133b927d214c76c0c9
Dylam <dylam2303@gmail.com>
2024-10-01 17:04:38 +0100

    no se 9

---
+++ cliente/practica-set-y-map/ejercicio2-de-set.js
@@ -0,0 +1,18 @@
+// Ejercicio 2:
+// Crea una función que recibe 3 parámetros (min, max, num) 
+// todos ellos enteros y devuelve un array de num elementos que son números elegidos al azar 
+// entre min y max pero que nunca se repiten. Por supuesto num no puede ser mayor que max - min.
+function getRandomIntInclusive(min, max, num){
+    let ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    let set = new Set();
+    while(set.size < num){
+        if(set.has(ranNum)){
+            ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        }else{
+            set.add(ranNum);
+        }
+    }
+}
+
+console.log(getRandomIntInclusive(1, 10, 5));
+console.log(getRandomIntInclusive(1, 100, 25));
